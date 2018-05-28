7eeef70e76963f4a1f7739526766e6f909a13452
xiesen110 <xiesen310@qq.com>
2018-05-28 20:51:58 +0800

    提交代码

---
+++ src/top/xiesen/llkgame/other/About.java
@@ -0,0 +1,17 @@
+package top.xiesen.llkgame.other;
+
+import top.xiesen.llkgame.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);// 布局文件引用about.xml
+
+	}
+
+}
